8579d37d88a74c888ecba9bd3ccd9cf80b8adf16
amotie <amotie@outlook.com>
2021-03-24 21:19:56 +0200

    Fixing Responsive

--- weds360/lib/components/raised_button_costem.dart
+++ weds360/lib/components/raised_button_costem.dart
@@ -12,17 +12,14 @@ class RaisedButtonCustem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // height: 60.0,
-
-      // padding: const EdgeInsets.only(
-      //     top: 16.0, bottom: 8.0, left: 20.0, right: 20.0),
-      child: RaisedButton(
-        color: Theme.of(context).primaryColor,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(ROUNDED * 2),
-        ),
-        onPressed: onPressed,
+    return RaisedButton(
+      color: Theme.of(context).primaryColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(ROUNDED * 2),
+      ),
+      onPressed: onPressed,
+      child: Padding(
+        padding: const EdgeInsets.all(kPADDING),
         child: Text(
           title,
           style: Theme.of(context).textTheme.button.copyWith(
