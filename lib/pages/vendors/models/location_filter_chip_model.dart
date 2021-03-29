5a6d7e86e68c623d4c433c4091fa6c6521dbba26
amotie <amotie@outlook.com>
2021-03-29 14:41:34 +0200

    Create filter view as described.

---
+++ lib/pages/vendors/models/location_filter_chip_model.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+
+class LocationFiltterChipModel {
+  String label;
+  bool isSelcted;
+  LocationFiltterChipModel({
+    @required this.label,
+    @required this.isSelcted,
+  });
+  
+}
